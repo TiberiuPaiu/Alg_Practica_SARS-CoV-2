eef0ee5979d75c9003a9f9a110234d3ce1218508
valcobe <44205158+valcobe@users.noreply.github.com>
2020-05-24 18:54:31 +0200

    parser fet

--- parser.py
+++ parser.py
@@ -1,71 +1,63 @@
+"""
+Pràctica Algorítmica 2019/2020
+Participants:
+    Víctor Alcobé
+    Tibireu Paiu
+    Dand Marbà
+    Pau Agustí
+"""
+
 import csv
-import sys
+
 
 class Parser:
+    """
+    Parser to read just the info we need
+    """
 
     def __init__(self, inputfile):
         """
-        accessions_geo_location: two lists, the one have the accession and the
-                                other the country which it belongs
+        geolocation_accessions_length: a list of three lists, one with the geoclocation,
+        the other with accessions and the last one with the lengths
 
         countries: list of all the countries that appears on the database
 
         accessions: list of lists, first element of every list is the country,
-                    and all the others are the accessions
+        and all the others are the accessions
         """
-        self.accessions_geo_location = [[], []]
-        self.countries = []
-        self.accessions = []
+        self.dades = []
         self.parse(inputfile)
-        self.convert_accessions()
-        self.cut_countries()
 
     def parse(self, inputfile):
+        """
+        Read from the csv file the needed info
+        """
 
-        """ Read from the csv file the needed info"""
-
-        with open(inputfile, newline = '') as csvfile:
+        with open(inputfile, newline='') as csvfile:
             reader = csv.DictReader(csvfile)
-
+            i = 0
             for row in reader:
                 country = row['Geo_Location'].split(":")
-                country = country[0]
-
-                if country not in self.countries:
-                    self.countries.append(country)
-
-                self.accessions_geo_location[1].append(row['Accession'])
-                self.accessions_geo_location[0].append(country)
-
-        self.countries = list(filter(None, self.countries))
-        self.accessions = self.countries
-
-    def cut_countries(self):
-
-        """ Split accessions and classfied into countries.
-            Returns a list of lists, first element of every list
-            is the country, and all the others are accessions"""
-
-        for index_accession in range(len(self.accessions_geo_location[0])):
-            for index_countries in range(len(self.countries)):
-                if self.countries[index_countries] == self.accessions_geo_location[0][index_accession]:
-                    self.accessions[index_countries].append(self.accessions_geo_location[1][index_accession])
+                self.dades.append(row)
+                self.dades[i]['Geo_Location'] = country[0]
+                print(self.dades[i]['Geo_Location'])
 
 
-        #print(self.accessions)
-        #print(self.countries)
+def parser_fasta(fasta_file):
+    """
+    Extract the sequence of the Fasta files
+    max length of a sequence = 1000
+    """
 
-    def convert_accessions(self):
+    with open("FASTA/" + fasta_file, "r") as file:
+        sequence = ""
+        line = file.readline()
+        line = file.readline()
 
-        """ Used to convert a list into a list of lists needed for the append
-            of the cut_countries() function"""
+        while line:
+            line = line.rstrip('\n')
+            sequence += line
+            line = file.readline()
 
-        result = []
-        for element in self.accessions:
-            temp = element.split(', ')
-            result.append(temp)
-
-        self.accessions = result
-
-#if __name__ == '__main__':
-    #parser = Parser(sys.argv[1])
+    sequence = sequence[:100]
+    return sequence
